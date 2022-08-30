fe02ac21ec26a909e0ada1b31a7efcf782bdbadc
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-08-30 11:17:41 -0400

    Insertion Sort Psuedo Code

--- algorithms/sorts/insertionSort/index.js
+++ algorithms/sorts/insertionSort/index.js
@@ -13,12 +13,18 @@
   - the left portion will become sorted, the right portion
       (that hasn't been iterated over yet) is unsorted.
   
+      1. consider the first item in any list as "sorted"
+      2. starting at index 1, check if value is < than value to the left
+        -if yes, swap values
+        -repeat this, until value is in the correct position
+      3. move on to next index
+      4. return when finished
 */
 
 const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const expected = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 /**
  * Sorts the given array in-place.
