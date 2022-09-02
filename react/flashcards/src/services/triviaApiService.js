3de530530abcb3302bd11535422f70fc9692e69d
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-09-02 15:29:32 -0400

    Updated Service for use of baseURL

--- react/flashcards/src/services/triviaApiService.js
+++ react/flashcards/src/services/triviaApiService.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
 // this instance of axios will concat given urls to the end of baseURL
-// DO NOT USE THIS, NEED TO REFACTOR TO CONSIDER BASEURL
-// const http = axios.create({
-//     baseURl: `https://opentdb.com/api.php`,
-// })
+const http = axios.create({
+    baseURL: `https://opentdb.com/api.php`,
+    //baseURl != baseURL, make sure you use autocomplete when available to avoid spelling mistakes!
+})
 
 export const getQuestions = (queryParams) => {
     // in a perfect world, it would be easier for the user to pass in category name
     // rather than category id (and have us figure out the id in this function)
     const { amount, category, difficulty, type } = queryParams
     
-    return axios
+    return http
         .get(
-            `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`
+            `?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`
         )
         .then(res => {
             // our component only cares about the data
