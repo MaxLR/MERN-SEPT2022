4e43f0b7f85676a8eddba39512fc85d3fca145e6
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-09-21 13:19:48 -0400

    Rehash Pseudo code

--- algorithms/strings/rehash.js
+++ algorithms/strings/rehash.js
@@ -13,4 +13,27 @@ const expected1 = "a184b70c42";
  * @param {string} s An incorrectly hashed string.
  * @returns {string} The correctly rehashed string alphabetized.
  */
-function rehash(s) {}
+const rehash = (s) => {}
+
+
+//create a function that takes in a string
+//create an output string variable
+//create a letterCount dictionary
+//create a currentLetter variable
+//create a numberString variable
+
+//loop through input string
+    //if current character isNaN
+        //if currentLetter exists
+            //if currentLetter already exists in dictionary
+                //add numberString to the value already stored for currentLetter
+            //if currentLetter DOESN'T exist in dictionary
+                //initialize that value to current value stored in numberString
+        //set numberString to ""
+        //set currentLetter to char
+    //if character IS a number
+        //push current character into number string
+
+// get our keys sorted alphabetically
+// for each key, concat the key + the value onto outputString
+// return outputString
